2c573dae9cfa812df601f901c4cd60d7ade35a6b
st0012 <stan001212@gmail.com>
2020-09-19 00:28:55 +0800

    Revert "Merge branch 'master' of github.com:getsentry/raven-ruby into fix-384-for-sidekiq-and-delayedjob"
    This reverts commit 22b8caf04848c6121ce249c9fbb4ec0b806027e4, reversing
    changes made to a1b3b156bed46d66118c1ba03cfae5043df29f26.

--- sentry-ruby/lib/sentry/version.rb
+++
@@ -1,3 +0,0 @@
-module Sentry
-  VERSION = "0.1.0"
-end
--- sentry-ruby/spec/sentry_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Sentry do
-  it "has a version number" do
-    expect(Sentry::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
